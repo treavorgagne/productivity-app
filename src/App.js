639e76db0c5ff71a5493a32a4eda203f05374714
Treavor Gagne <gagne.16@hotmail.com>
2022-08-24 23:51:08 -0700

    fixed mobile view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
+import { ChakraProvider, Box, VStack, theme } from '@chakra-ui/react';
 import { Timer } from './components/Timer';
 import { List } from './components/List';
 import { Header } from './components/Header';
@@ -8,15 +8,13 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack>
-            <Header />
-            <Box w={'100%'} mx={'auto'} h={'93vh'}>
-              <Timer />
-              <List />
-            </Box>
-          </VStack>
-        </Grid>
+        <VStack minH="100vh" p={3}>
+          <Header />
+          <Box w={'100%'} mx={'auto'} h={'93vh'}>
+            <Timer />
+            <List />
+          </Box>
+        </VStack>
       </Box>
     </ChakraProvider>
   );
